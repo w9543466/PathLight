b56b73ca66b2434879a6a3345d51e08739eaca39
w9543466 <w9543466@live.tees.ac.uk>
2022-10-28 13:49:56 +0100

    Path Light_App_Initial Stage

---
+++ app/src/main/java/uk/ac/tees/w9543466/pathlight/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.w9543466.pathlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
